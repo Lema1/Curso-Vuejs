1c3231cfb53ba38c15ecdaec00dd3b60e16daa56
Alvaro <contact@fastweb.com>
2018-12-18 17:58:22 -0300

    Mi primir Commit

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el : '#app',
+    data : {
+        lista : [
+            {nombre: 'pedro',promedio: '10', estado: false},
+            {nombre: 'jorge',promedio: '13', estado: true},
+            {nombre: 'carlos',promedio: '15', estado: true},
+        ],
+    }
+})
