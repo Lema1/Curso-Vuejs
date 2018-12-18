f44af4e7e257187b4413990a4a922d31639aa992
Alvaro <contact@fastweb.com>
2018-12-18 19:37:32 -0300

    Competado punto 11

--- js/principal.js
+++ js/principal.js
@@ -2,9 +2,24 @@ var app = new Vue({
     el : '#app',
     data : {
         lista : [
-            {nombre: 'pedro',promedio: '10', estado: false},
-            {nombre: 'jorge',promedio: '13', estado: true},
-            {nombre: 'carlos',promedio: '15', estado: true},
         ],
+        nombre : "",
+        promedio : ""
+    },
+    methods : {
+        agregarNota: function (){
+            estadoA=false;
+            if(this.promedio>=12.5){
+                estadoA=true;
+            }
+            if(this.nombre!="" && this.promedio!=""){
+                this.lista.push({nombre:this.nombre, promedio:this.promedio , estado:estadoA});
+                this.nombre="";
+                this.promedio="";
+            }
+            else{
+                alert ("campos vacios");
+            }
+        }
     }
 })
